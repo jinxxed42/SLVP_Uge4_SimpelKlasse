16c70af74a71b81a9d639598d5c0574c9b36e269
Claus Damgaard <jinxxed@outlook.com>
2023-03-21 22:21:57 +0100

    Add project files.

---
+++ Beer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLVP_Uge4_SimpelKlasse
+{
+    internal class Beer
+    {
+        public string name;
+        public int size;
+        public int price;
+
+        public Beer(string name, int size, int price)
+        {
+            this.name = name;
+            this.size = size;
+            this.price = price;
+        }
+
+
+
+        public string ToString()
+        {
+            return "Navn : " + name + " Størrelse: " + size + "cL. Pris: " + price + "kr.";
+        }
+
+        public int GetPrice()
+        {
+            return price;
+        }
+
+    }
+}
